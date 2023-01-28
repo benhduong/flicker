f776c27dda8a1315f705be118e3a01773cf3107e
Kazen Gallman <kazar444@icloud.com>
2023-01-28 15:24:32 -0500

    ability to add basic tasks has been added

--- src/AddHabit.js
+++ src/AddHabit.js
@@ -1,15 +1,30 @@
 import logo from './logo.svg';
 import './Habit.css';
+import React, { useState } from 'react';
 
 function AddHabit(props) {
 
   let text = props.text
+  let setCurrHabits = props.setCurrHabits
+  let currHabits = props.currHabits
+
+  const [inputText, setInputText] = useState("")
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      setCurrHabits([...currHabits, inputText])
+    }
+  };
 
   return (
     <div className="addHabit">
       <div className="button-27 addHabitLine">
-        <input type="text" placeholder='New Habit' className='habitInput'></input>
-        <img className="plusIcon" src={"/plus.svg"}></img>
+        <input type="text" placeholder='New Habit' className='habitInput' onChange={(e) => {
+          setInputText(e.target.value)
+        }} onKeyDown={handleKeyDown}></input>
+        <img className="plusIcon" src={"/plus.svg"} onClick={() => {
+          setCurrHabits([...currHabits, inputText])
+        }}></img>
       </div>
     </div>
   );
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './HomePage.css';
+import React, { useState } from 'react';
 
 import { getAuth, getRedirectResult, GoogleAuthProvider } from "firebase/auth";
 import AddHabit from './AddHabit';
@@ -28,7 +29,7 @@ import Habit from './Habit'
 
 function HomePage() {
 
-  let currHabits = ["Test Habit", "Another Habit"]
+  const [currHabits, setCurrHabits] = useState(["Test Habit", "Another Habit"])
 
   return (
     <div className="homepage">
@@ -36,7 +37,7 @@ function HomePage() {
       <h1>Name Of Wesbite</h1>
 
       <div className="habits">
-        <AddHabit text={"Add New Habit"}/>
+        <AddHabit text={"Add New Habit"} currHabits={currHabits} setCurrHabits={setCurrHabits}/>
         {currHabits.map((habitText, i) => <Habit text={habitText} key={i}/>)}
       </div>
       <div className="fix">CampFire Div
