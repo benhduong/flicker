29be193ffaa08535e4b3e62877753cf3189444d8
benhduong <benhduong@gmail.com>
2023-01-28 13:32:09 -0500

    login with google

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,6 +35,33 @@ const firebaseConfig = {
   measurementId: "G-SW3KZLST20",
 };
 
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user;
+    const q = query(collection(db, "users"), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const googleProvider = new GoogleAuthProvider();
+
+
+const auth = getAuth(app);
+const db = getFirestore();
+
+export { auth, db, signInWithGoogle };
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,38 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-
-const auth = getAuth();
-signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    // ...
-  })
-  .catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
+import React, { useEffect, useState } from "react";
+import { signInWithGoogle } from "./Firebase.js";
 
 
-
-
-function LoginPage() {
+function Login() {
   return (
-    <div className="loginpage">
-      
+    <div className="login">
+      <div className="login__container">
+
+        <button  onClick={signInWithGoogle}>
+          Login with Google
+        </button>
+      </div>
     </div>
   );
 }
 
-export default LoginPage;
+export default Login;
