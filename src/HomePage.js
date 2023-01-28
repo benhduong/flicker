5a7c5e1e77c5659ebc48f307dbd10a7dc890ea61
Kazen Gallman <kazar444@icloud.com>
2023-01-28 12:33:17 -0500

    added homepage and loginpage

---
+++ src/HomePage.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/LoginPage.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function LoginPage() {
+  return (
+    <div className="loginpage">
+      
+    </div>
+  );
+}
+
+export default LoginPage;
