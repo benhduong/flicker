eaaaeb61f9d7c0b4ada405b0f07484e668d4b567
benhduong <benhduong@gmail.com>
2023-01-29 10:42:52 -0500

    fire levels

---
+++ src/components/MediumFire.js
@@ -0,0 +1,20 @@
+import "./Fire.css";
+
+function MediumFire() {
+  return (
+    <div className="content">
+      <div className="light"></div>
+      <div className="fire">
+        <div className="medium"></div>
+        <div className="front"></div>
+      </div>
+      <div className="wood">
+        <div className="stick"></div>
+        <div className="stick"></div>
+        <div className="stick"></div>
+      </div>
+    </div>
+  );
+}
+
+export default MediumFire;
---
+++ src/components/SmallFire.js
@@ -0,0 +1,19 @@
+import "./Fire.css";
+
+function SmallFire() {
+  return (
+    <div className="content">
+      <div className="light"></div>
+      <div className="fire">
+        <div className="front"></div>
+      </div>
+      <div className="wood">
+        <div className="stick"></div>
+        <div className="stick"></div>
+        <div className="stick"></div>
+      </div>
+    </div>
+  );
+}
+
+export default SmallFire;
